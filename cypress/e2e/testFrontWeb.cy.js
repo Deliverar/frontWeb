84843f90b71b6b40527140c25aa450ac71254616
Lucas Miño <lucas@xoolix.com>
2023-10-27 02:46:26 -0300

    cypress

---
+++ cypress/e2e/testFrontWeb.cy.js
@@ -0,0 +1,8 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000')
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit('http://localhost:3000/home');
+    /* ==== End Cypress Studio ==== */
+  })
+})
