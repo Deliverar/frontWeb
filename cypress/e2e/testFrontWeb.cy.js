37f1d8c793645865ccdd0ce9d8bdc550203644f0
Lucas Miño <lucas@xoolix.com>
2023-10-27 02:58:23 -0300

    tests

--- cypress/e2e/testFrontWeb.cy.js
+++ cypress/e2e/testFrontWeb.cy.js
@@ -1,8 +1,19 @@
 describe('template spec', () => {
-  it('passes', () => {
+    /* ==== Test Created with Cypress Studio ==== */
+  it('test_1', function() {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit('http://localhost:3000/home');
+    cy.get('[href="/deepracerdashboard"] > img').click();
+    cy.get('span').click({force: true});
+    cy.get('.header_logo__HqFVt > img').click();
+    /* ==== End Cypress Studio ==== */
+  });
+  it('test_2', () => {
     cy.visit('http://localhost:3000')
     /* ==== Generated with Cypress Studio ==== */
     cy.visit('http://localhost:3000/home');
+    cy.visit('http://localhost:3000/deepracerdashboard');
+    cy.visit('http://localhost:3000/');
     /* ==== End Cypress Studio ==== */
   })
 })
