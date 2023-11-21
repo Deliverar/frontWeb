aadf17b4874042d684397246427c75faa9d4f6d8
FerCardozoX <cardozofernando18@gmail.com>
2023-11-21 02:24:38 -0300

    Boton y tabla de usuarios en deepracer

--- src/pages/DeepRacerDashboard.js
+++ src/pages/DeepRacerDashboard.js
@@ -10,6 +10,8 @@ function DeepRacerDashboard() {
   }
 
   const [raceData, setRaceData] = useState([]);
+  const [raceDataUsuarios, setRaceDataUsuarios] = useState([]);
+  const [editMode, setEditMode] = useState(false);
  
 function convertMillisecondsToTime(milliseconds) {
   const totalSeconds = Math.floor(milliseconds / 1000);
@@ -39,12 +41,427 @@ function convertMillisecondsToDateTimeday(milliseconds) {
     axios.get('http://abmpersonalinternoapi.deliver.ar/api/deepracer/leaderboard')
       .then(response => {
         setRaceData(response.data);
+
+        const usuarios = [
+          {
+            Alias: "Racer1",
+            NombreApellido: "",
+            DNI: "",
+          },{
+            Alias: "Racer2",
+            NombreApellido: "",
+            DNI: "",
+          },{
+            Alias: "Racer3",
+            NombreApellido: "",
+            DNI: "",
+          },{
+            Alias: "Racer4",
+            NombreApellido: "",
+            DNI: "",
+          },{
+            Alias: "Racer5",
+            NombreApellido: "",
+            DNI: "",
+          },{
+            Alias: "Racer6",
+            NombreApellido: "",
+            DNI: "",
+          },{
+            Alias: "Racer7",
+            NombreApellido: "",
+            DNI: "",
+          },{
+            Alias: "Racer8",
+            NombreApellido: "",
+            DNI: "",
+          },
+        ];
+        setRaceDataUsuarios(usuarios);
       })
       .catch(error => {
+        /*const usuarios = [
+          {
+            Alias: "Racer8",
+            BestLapTime: 122000,
+            SubmissionTime: 1668435600000, // Fecha en milisegundos (por ejemplo, 15 de febrero de 2023)
+            LapCount: 3,
+            CollisionCount: 0,
+            OffTrackCount: 0,
+            SubmissionCount: 1,
+          },{
+            Alias: "Racer8",
+            BestLapTime: 122000,
+            SubmissionTime: 1668435600000, // Fecha en milisegundos (por ejemplo, 15 de febrero de 2023)
+            LapCount: 3,
+            CollisionCount: 0,
+            OffTrackCount: 0,
+            SubmissionCount: 1,
+          },{
+            Alias: "Racer8",
+            BestLapTime: 122000,
+            SubmissionTime: 1668435600000, // Fecha en milisegundos (por ejemplo, 15 de febrero de 2023)
+            LapCount: 3,
+            CollisionCount: 0,
+            OffTrackCount: 0,
+            SubmissionCount: 1,
+          },{
+            Alias: "Racer8",
+            BestLapTime: 122000,
+            SubmissionTime: 1668435600000, // Fecha en milisegundos (por ejemplo, 15 de febrero de 2023)
+            LapCount: 3,
+            CollisionCount: 0,
+            OffTrackCount: 0,
+            SubmissionCount: 1,
+          },{
+            Alias: "Racer8",
+            BestLapTime: 122000,
+            SubmissionTime: 1668435600000, // Fecha en milisegundos (por ejemplo, 15 de febrero de 2023)
+            LapCount: 3,
+            CollisionCount: 0,
+            OffTrackCount: 0,
+            SubmissionCount: 1,
+          },{
+            Alias: "Racer8",
+            BestLapTime: 122000,
+            SubmissionTime: 1668435600000, // Fecha en milisegundos (por ejemplo, 15 de febrero de 2023)
+            LapCount: 3,
+            CollisionCount: 0,
+            OffTrackCount: 0,
+            SubmissionCount: 1,
+          },{
+            Alias: "Racer1",
+            BestLapTime: 120000,
+            SubmissionTime: 1669040400000, // Fecha en milisegundos (por ejemplo, 22 de febrero de 2023)
+            LapCount: 5,
+            CollisionCount: 2,
+            OffTrackCount: 1,
+            SubmissionCount: 1,
+          },{
+            Alias: "Racer1",
+            BestLapTime: 120000,
+            SubmissionTime: 1669040400000, // Fecha en milisegundos (por ejemplo, 22 de febrero de 2023)
+            LapCount: 5,
+            CollisionCount: 2,
+            OffTrackCount: 1,
+            SubmissionCount: 1,
+          },{
+            Alias: "Racer1",
+            BestLapTime: 120000,
+            SubmissionTime: 1669040400000, // Fecha en milisegundos (por ejemplo, 22 de febrero de 2023)
+            LapCount: 5,
+            CollisionCount: 2,
+            OffTrackCount: 1,
+            SubmissionCount: 1,
+          },{
+            Alias: "Racer1",
+            BestLapTime: 120000,
+            SubmissionTime: 1669040400000, // Fecha en milisegundos (por ejemplo, 22 de febrero de 2023)
+            LapCount: 5,
+            CollisionCount: 2,
+            OffTrackCount: 1,
+            SubmissionCount: 1,
+          },{
+            Alias: "Racer1",
+            BestLapTime: 120000,
+            SubmissionTime: 1669040400000, // Fecha en milisegundos (por ejemplo, 22 de febrero de 2023)
+            LapCount: 5,
+            CollisionCount: 2,
+            OffTrackCount: 1,
+            SubmissionCount: 1,
+          },{
+            Alias: "Racer1",
+            BestLapTime: 120000,
+            SubmissionTime: 1669040400000, // Fecha en milisegundos (por ejemplo, 22 de febrero de 2023)
+            LapCount: 5,
+            CollisionCount: 2,
+            OffTrackCount: 1,
+            SubmissionCount: 1,
+          },{
+            Alias: "Racer1",
+            BestLapTime: 120000,
+            SubmissionTime: 1669040400000, // Fecha en milisegundos (por ejemplo, 22 de febrero de 2023)
+            LapCount: 5,
+            CollisionCount: 2,
+            OffTrackCount: 1,
+            SubmissionCount: 1,
+          },{
+            Alias: "Racer1",
+            BestLapTime: 120000,
+            SubmissionTime: 1669040400000, // Fecha en milisegundos (por ejemplo, 22 de febrero de 2023)
+            LapCount: 5,
+            CollisionCount: 2,
+            OffTrackCount: 1,
+            SubmissionCount: 1,
+          },{
+            Alias: "Racer1",
+            BestLapTime: 120000,
+            SubmissionTime: 1669040400000, // Fecha en milisegundos (por ejemplo, 22 de febrero de 2023)
+            LapCount: 5,
+            CollisionCount: 2,
+            OffTrackCount: 1,
+            SubmissionCount: 1,
+          },{
+            Alias: "Racer1",
+            BestLapTime: 120000,
+            SubmissionTime: 1669040400000, // Fecha en milisegundos (por ejemplo, 22 de febrero de 2023)
+            LapCount: 5,
+            CollisionCount: 2,
+            OffTrackCount: 1,
+            SubmissionCount: 1,
+          },
+          {
+            Alias: "Racer2",
+            BestLapTime: 115000,
+            SubmissionTime: 1668954000000, // Fecha en milisegundos (por ejemplo, 21 de febrero de 2023)
+            LapCount: 4,
+            CollisionCount: 0,
+            OffTrackCount: 0,
+            SubmissionCount: 1,
+          },
+          {
+            Alias: "Racer3",
+            BestLapTime: 110000,
+            SubmissionTime: 1668867600000, // Fecha en milisegundos (por ejemplo, 20 de febrero de 2023)
+            LapCount: 6,
+            CollisionCount: 1,
+            OffTrackCount: 0,
+            SubmissionCount: 1,
+          },
+          {
+            Alias: "Racer4",
+            BestLapTime: 125000,
+            SubmissionTime: 1668781200000, // Fecha en milisegundos (por ejemplo, 19 de febrero de 2023)
+            LapCount: 3,
+            CollisionCount: 3,
+            OffTrackCount: 2,
+            SubmissionCount: 1,
+          },
+          {
+            Alias: "Racer5",
+            BestLapTime: 118000,
+            SubmissionTime: 1668694800000, // Fecha en milisegundos (por ejemplo, 18 de febrero de 2023)
+            LapCount: 5,
+            CollisionCount: 0,
+            OffTrackCount: 1,
+            SubmissionCount: 1,
+          },
+          {
+            Alias: "Racer6",
+            BestLapTime: 130000,
+            SubmissionTime: 1668608400000, // Fecha en milisegundos (por ejemplo, 17 de febrero de 2023)
+            LapCount: 4,
+            CollisionCount: 1,
+            OffTrackCount: 0,
+            SubmissionCount: 1,
+          },{
+            Alias: "Racer1",
+            BestLapTime: 120000,
+            SubmissionTime: 1669040400000, // Fecha en milisegundos (por ejemplo, 22 de febrero de 2023)
+            LapCount: 5,
+            CollisionCount: 2,
+            OffTrackCount: 1,
+            SubmissionCount: 1,
+          },{
+            Alias: "Racer1",
+            BestLapTime: 120000,
+            SubmissionTime: 1669040400000, // Fecha en milisegundos (por ejemplo, 22 de febrero de 2023)
+            LapCount: 5,
+            CollisionCount: 2,
+            OffTrackCount: 1,
+            SubmissionCount: 1,
+          },{
+            Alias: "Racer1",
+            BestLapTime: 120000,
+            SubmissionTime: 1669040400000, // Fecha en milisegundos (por ejemplo, 22 de febrero de 2023)
+            LapCount: 5,
+            CollisionCount: 2,
+            OffTrackCount: 1,
+            SubmissionCount: 1,
+          },{
+            Alias: "Racer1",
+            BestLapTime: 120000,
+            SubmissionTime: 1669040400000, // Fecha en milisegundos (por ejemplo, 22 de febrero de 2023)
+            LapCount: 5,
+            CollisionCount: 2,
+            OffTrackCount: 1,
+            SubmissionCount: 1,
+          },{
+            Alias: "Racer1",
+            BestLapTime: 120000,
+            SubmissionTime: 1669040400000, // Fecha en milisegundos (por ejemplo, 22 de febrero de 2023)
+            LapCount: 5,
+            CollisionCount: 2,
+            OffTrackCount: 1,
+            SubmissionCount: 1,
+          },{
+            Alias: "Racer1",
+            BestLapTime: 120000,
+            SubmissionTime: 1669040400000, // Fecha en milisegundos (por ejemplo, 22 de febrero de 2023)
+            LapCount: 5,
+            CollisionCount: 2,
+            OffTrackCount: 1,
+            SubmissionCount: 1,
+          },{
+            Alias: "Racer1",
+            BestLapTime: 120000,
+            SubmissionTime: 1669040400000, // Fecha en milisegundos (por ejemplo, 22 de febrero de 2023)
+            LapCount: 5,
+            CollisionCount: 2,
+            OffTrackCount: 1,
+            SubmissionCount: 1,
+          },{
+            Alias: "Racer1",
+            BestLapTime: 120000,
+            SubmissionTime: 1669040400000, // Fecha en milisegundos (por ejemplo, 22 de febrero de 2023)
+            LapCount: 5,
+            CollisionCount: 2,
+            OffTrackCount: 1,
+            SubmissionCount: 1,
+          },{
+            Alias: "Racer1",
+            BestLapTime: 120000,
+            SubmissionTime: 1669040400000, // Fecha en milisegundos (por ejemplo, 22 de febrero de 2023)
+            LapCount: 5,
+            CollisionCount: 2,
+            OffTrackCount: 1,
+            SubmissionCount: 1,
+          },{
+            Alias: "Racer1",
+            BestLapTime: 120000,
+            SubmissionTime: 1669040400000, // Fecha en milisegundos (por ejemplo, 22 de febrero de 2023)
+            LapCount: 5,
+            CollisionCount: 2,
+            OffTrackCount: 1,
+            SubmissionCount: 1,
+          },
+          {
+            Alias: "Racer2",
+            BestLapTime: 115000,
+            SubmissionTime: 1668954000000, // Fecha en milisegundos (por ejemplo, 21 de febrero de 2023)
+            LapCount: 4,
+            CollisionCount: 0,
+            OffTrackCount: 0,
+            SubmissionCount: 1,
+          },
+          {
+            Alias: "Racer3",
+            BestLapTime: 110000,
+            SubmissionTime: 1668867600000, // Fecha en milisegundos (por ejemplo, 20 de febrero de 2023)
+            LapCount: 6,
+            CollisionCount: 1,
+            OffTrackCount: 0,
+            SubmissionCount: 1,
+          },
+          {
+            Alias: "Racer4",
+            BestLapTime: 125000,
+            SubmissionTime: 1668781200000, // Fecha en milisegundos (por ejemplo, 19 de febrero de 2023)
+            LapCount: 3,
+            CollisionCount: 3,
+            OffTrackCount: 2,
+            SubmissionCount: 1,
+          },
+          {
+            Alias: "Racer5",
+            BestLapTime: 118000,
+            SubmissionTime: 1668694800000, // Fecha en milisegundos (por ejemplo, 18 de febrero de 2023)
+            LapCount: 5,
+            CollisionCount: 0,
+            OffTrackCount: 1,
+            SubmissionCount: 1,
+          },
+          {
+            Alias: "Racer6",
+            BestLapTime: 130000,
+            SubmissionTime: 1668608400000, // Fecha en milisegundos (por ejemplo, 17 de febrero de 2023)
+            LapCount: 4,
+            CollisionCount: 1,
+            OffTrackCount: 0,
+            SubmissionCount: 1,
+          },
+          {
+            Alias: "Racer7",
+            BestLapTime: 112000,
+            SubmissionTime: 1668522000000, // Fecha en milisegundos (por ejemplo, 16 de febrero de 2023)
+            LapCount: 5,
+            CollisionCount: 2,
+            OffTrackCount: 1,
+            SubmissionCount: 1,
+          },
+          {
+            Alias: "Racer8",
+            BestLapTime: 122000,
+            SubmissionTime: 1668435600000, // Fecha en milisegundos (por ejemplo, 15 de febrero de 2023)
+            LapCount: 3,
+            CollisionCount: 0,
+            OffTrackCount: 0,
+            SubmissionCount: 1,
+          },
+          // ... Agrega más elementos si es necesario
+        ];
+        setRaceData(exampleRaceData);*/
+        const usuarios = [
+          {
+            Alias: "Racer1",
+            NombreApellido: "",
+            DNI: "",
+          },{
+            Alias: "Racer2",
+            NombreApellido: "",
+            DNI: "",
+          },{
+            Alias: "Racer3",
+            NombreApellido: "",
+            DNI: "",
+          },{
+            Alias: "Racer4",
+            NombreApellido: "",
+            DNI: "",
+          },{
+            Alias: "Racer5",
+            NombreApellido: "",
+            DNI: "",
+          },{
+            Alias: "Racer6",
+            NombreApellido: "",
+            DNI: "",
+          },{
+            Alias: "Racer7",
+            NombreApellido: "",
+            DNI: "",
+          },{
+            Alias: "Racer8",
+            NombreApellido: "",
+            DNI: "",
+          },
+        ];
+        setRaceDataUsuarios(usuarios);
         console.error('Error al obtener datos de la API', error);
     
       });
   }, []);
+
+  const handleEdit = (entry) => {
+
+    console.log('Editar entrada:', entry);
+
+    setEditMode(false)
+  };
+
+  const notificarCarrera = () => {
+
+
+    axios.post('http://abmpersonalinternoapi.deliver.ar/api/notificarCarrera')
+      .then(response => {
+        
+        console.log('Carrera notificada con éxito');
+      })
+      .catch(error => {
+        
+        console.error('Error al notificar carrera');
+      });
+  };
 
   return (
     <div className={styles.background}>
@@ -65,7 +482,9 @@ function convertMillisecondsToDateTimeday(milliseconds) {
         </svg>
 
         <span>Volver</span>
+        
       </div>
+      
       <div className={styles.content}>
         <section className={styles.deepSection}>
           <div>
@@ -92,6 +511,8 @@ function convertMillisecondsToDateTimeday(milliseconds) {
             </div>
           </div>
         </section>
+        <button style={{ width: '300px', height:'200px'  }} onClick={notificarCarrera}>Notificar carrera</button>
+        <div className={styles.scrollableTable}>
         <table className={styles.table}>
           <thead>
             <tr>
@@ -120,8 +541,46 @@ function convertMillisecondsToDateTimeday(milliseconds) {
             ))}
           </tbody>
         </table>
+    
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>   Alias   </th>
+              <th>   Nombre y Apellido   </th>
+              <th>   DNI   </th>
+              <th>      </th>
+            </tr>
+          </thead>
+          <tbody>
+            {raceDataUsuarios.map((entry, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{entry.Alias}</td>
+                <td>{entry.NombreApellido}</td>
+                <td>{entry.DNI}</td>
+                <td>
+                    {editMode ? (
+              <>
+                <button onClick={() => handleEdit(entry)}>Guardar</button>
+                <button onClick={() => setEditMode(false)}>Cancelar</button>
+              </>
+            ) : (
+              <button onClick={() => setEditMode(true)}>EDITAR</button>
+            )}
+          </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        </div>
+        <div className='espacio'>
+        </div>
+        <div className='espacio1'>
+        </div>
+        </div>
       </div>
-    </div>
+    
   );
 }
 
